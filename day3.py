2d3864302a783a8a2751f27347158ac222f3c877
Rol& <rolandbit808@protonmail.com>
2024-12-11 21:52:27 +0000

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -15,4 +15,19 @@ def part1(input: TextIOWrapper):
 
 
 def part2(input: TextIOWrapper):
-    pass
+    program = input.read()
+
+    mul_pattern = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+
+    multi_enabled = True
+    multi: int = 0
+    for match in mul_pattern.finditer(program):
+        # whole match
+        if match.group(0) == "do()":
+            multi_enabled = True
+        elif match.group(0) == "don't()":
+            multi_enabled = False
+        elif multi_enabled:
+            multi = multi + (int(match.group(1)) * int(match.group(2)))
+
+    print(f"{multi=}")
